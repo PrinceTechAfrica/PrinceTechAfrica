eba273db4dd617efe1445021e7d33ecd0636beb9
PrinceTechAfrica <105421666+PrinceTechAfrica@users.noreply.github.com>
2022-07-04 11:06:43 +0100

    Add files via upload
    Assignment on how to create A table and Insert Data into it

---
+++ assignment 8&9.sql
@@ -0,0 +1,9 @@
+use assignment_db;
+insert into patient(id,name,DOB,contact,address,marital_status)values
+('1','oluchi','1990-05-23','08163494157','arab road','single'),
+('2','amaka','1990-05-23','08163494157','arab road','single');
+
+ 
+
+
+ 
